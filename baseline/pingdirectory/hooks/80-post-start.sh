a5ce505c2920fcc6b74bb525dcfbe8c5060964fd
Terry Sigle <terrysigle@pingidentity.com>
2019-12-16 15:26:21 -0600

    multi-cluster

--- baseline/pingdirectory/hooks/80-post-start.sh
+++ baseline/pingdirectory/hooks/80-post-start.sh
@@ -26,11 +26,13 @@ echo "
 Updating the Server Instance hostname/ldaps-port:
          instance: ${_podInstanceName}
          hostname: ${_podHostname}
+        ldap-port: ${_podLdapPort}
        ldaps-port: ${_podLdapsPort}"
 
 dsconfig set-server-instance-prop --no-prompt --quiet \
     --instance-name "${_podInstanceName}" \
     --set hostname:${_podHostname} \
+    --set ldap-port:${_podLdapPort} \
     --set ldaps-port:${_podLdapsPort}
 
 _updateServerInstanceResult=$?
@@ -53,11 +55,14 @@ fi
 
 echo "Running dsreplication enable"
 
-echo "Running ldapsearch test on SEED Server (${_seedInstanceName})"
-echo "        ${_seedHostname}:${_seedLdapsPort}"
-waitUntilLdapUp "${_seedHostname}" "${_seedLdapsPort}" ""
+_tmpPort="${_seedLdapPort}"
+test "${LDAP_SECURITY}" == "ssl" && _tmpPort="${_seedLdapsPort}"
 
-_masterTopologyInstance=$(ldapsearch --host "${_seedHostname}" --port "${_seedLdapsPort}" --terse --outputFormat json -b "cn=Mirrored subtree manager for base DN cn_Topology_cn_config,cn=monitor" -s base objectclass=* master-instance-name | jq -r .attributes[].values[])
+echo "Running ldapsearch test on SEED Server (${_seedInstanceName})"
+echo "        ${_seedHostname}:${_tmpPort}"
+waitUntilLdapUp "${_seedHostname}" "${_tmpPort}" ""
+
+_masterTopologyInstance=$(ldapsearch --host "${_seedHostname}" --port "${_tmpPort}" --terse --outputFormat json -b "cn=Mirrored subtree manager for base DN cn_Topology_cn_config,cn=monitor" -s base objectclass=* master-instance-name | jq -r .attributes[].values[])
 
 printf "
 #############################################
@@ -70,16 +75,21 @@ printf "
 #############################################
 " "SEED Server" "POD Server" "${_seedHostname}:${_seedReplicationPort}" "${_podHostname}:${_podReplicationPort}"
 
+_conn1="--port1 ${_seedLdapPort} --startTLS1"
+_conn2="--port2 ${_seedLdapPort} --startTLS2"
+test "${LDAP_SECURITY}" == "ssl" && _conn1="--port1 ${_seedLdapsPort} --useSSL1"
+test "${LDAP_SECURITY}" == "ssl" && _conn2="--port2 ${_seedLdapsPort} --useSSL2"
+
 dsreplication enable \
       --retryTimeoutSeconds ${RETRY_TIMEOUT_SECONDS} \
       --trustAll \
       --host1 "${_seedHostname}" \
-      --port1 "${_seedLdapsPort}" --useSSL1 \
+      ${conn1} \
       --replicationPort1 "${_seedReplicationPort}" \
       --bindDN1 "${ROOT_USER_DN}" --bindPasswordFile1 "${ROOT_USER_PASSWORD_FILE}" \
       \
       --host2 "${_podHostname}" \
-      --port2 "${_podLdapsPort}" --useSSL2 \
+      ${conn2} \
       --replicationPort2 "${_podReplicationPort}" \
       --bindDN2 "${ROOT_USER_DN}" --bindPasswordFile2 "${ROOT_USER_PASSWORD_FILE}" \
       \
@@ -90,30 +100,33 @@ dsreplication enable \
       --enableDebug --globalDebugLevel verbose
 
 _replEnableResult=$?
-echo "Replication enable for POD Server ${_podHostname}:${_podLdapsPort} result=${_replEnableResult}"
+echo "Replication enable for POD Server result=${_replEnableResult}"
 
 if test ${_replEnableResult} -ne 0; then
     echo "Not running dsreplication initialize since enable failed with a non-successful return code"
     exit ${_replEnableResult}
 fi
 
-echo "Getting Topology from SEED Server: ${_seedHostname}:${_seedLdapsPort}"
+echo "Getting Topology from SEED Server"
 rm -rf "${TOPOLOGY_FILE}"
 manage-topology export \
     --hostname "${_seedHostname}" \
-    --port "${_seedLdapsPort}" \
+    --port "${_tmpPort}" \
     --exportFilePath "${TOPOLOGY_FILE}"
 
 cat "${TOPOLOGY_FILE}"
 
-echo "Initializing replication on POD Server: ${_podHostname}:${_podLdapsPort}"
+_destConn="--portDestination ${_seedLdapPort} --startTLSDestination"
+test "${LDAP_SECURITY}" == "ssl" && _destConn="--portDestination ${_seedLdapsPort} --useSSLDestination"
+
+echo "Initializing replication on POD Server"
 dsreplication initialize \
       --retryTimeoutSeconds ${RETRY_TIMEOUT_SECONDS} \
       --trustAll \
       \
       --topologyFilePath "${TOPOLOGY_FILE}" \
       \
-      --hostDestination "${_podHostname}" --portDestination "${_podLdapsPort}" --useSSLDestination \
+      --hostDestination "${_podHostname}" ${_destConn} \
       \
       --baseDN "${USER_BASE_DN}" \
       --adminUID "${ADMIN_USER_NAME}" \
@@ -123,7 +136,7 @@ dsreplication initialize \
       --globalDebugLevel verbose
 
 _replInitResult=$?
-echo "Replication initialize for: ${_podHostname}:${_podLdapsPort} result=${_replInitResult}"
+echo "Replication initialize result=${_replInitResult}"
 
 # test ${_replInitResult} -eq 0 && touch "${REPL_SETUP_MARKER_FILE}"
 exit ${_replInitResult}
